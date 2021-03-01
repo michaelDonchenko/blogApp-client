ea712d05b31a744e6996ad08366fb23733c43ce7
michaelDonchenko <michael1994d@gmail.com>
2021-03-01 15:25:33 +0200

    react router with basic raoutes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </Router>
   )
 }
 
